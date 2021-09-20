e5f775a78fff37a4806cf7b00177af760cf5220d
royr2 <riddhiman.roy@gmail.com>
2021-09-20 07:50:42 +0530

    utf8 encoding

--- generate_sitemap.R
+++ generate_sitemap.R
@@ -4,4 +4,4 @@ for(f in list.files("content/post")){
   sitemap <- paste0(sitemap, paste0("https://rtichoke.netlify.app/post/", f, ".html"), "\n")
 }
 
-writeLines(sitemap, "public/sitemap.txt")
+writeLines(sitemap, "public/sitemap.txt", useBytes=T)
