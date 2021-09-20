fd26302b46549b8705d6309ceb12181a87b1e107
royr2 <riddhiman.roy@gmail.com>
2021-09-20 07:49:35 +0530

    Updated sitemap

--- generate_sitemap.R
+++ generate_sitemap.R
@@ -1,7 +1,7 @@
 sitemap <- ""
 
 for(f in list.files("content/post")){
-  sitemap <- paste0(sitemap, paste0("https://rtichoke.netlify.app/post/", f), "\n")
+  sitemap <- paste0(sitemap, paste0("https://rtichoke.netlify.app/post/", f, ".html"), "\n")
 }
 
 writeLines(sitemap, "public/sitemap.txt")
